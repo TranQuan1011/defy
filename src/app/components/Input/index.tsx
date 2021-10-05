21f33f44f3f7c15f4e2c9853f6a3cac0adfebfcb
Vu Thu Ha <havtt@dol-tech.com>
2021-10-05 15:37:25 +0700

    input component

---
+++ src/app/components/Input/index.tsx
@@ -0,0 +1,25 @@
+import { styled } from '@mui/material/styles';
+import { InputBase } from '@mui/material';
+
+export const FormInput = styled(InputBase)`
+  position: absolute;
+  font-style: normal;
+  width: 584px;
+  height: 44px;
+  border: 1px solid #74767B;
+  box-sizing: border-box;
+  border-radius: 22px;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 20px;
+  color: #FFFFFF;
+  background: none;
+`;
+
+export const Label = styled(InputBase)`
+  position: absolute;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 17px;
+  color: #FFFFFF;
--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 
+import { Label, FormInput } from 'app/components/Input/index';
+
 export function HomePage() {
   return (
     <>
@@ -9,6 +11,9 @@ export function HomePage() {
         <meta name="description" content="A Boilerplate application homepage" />
       </Helmet>
       <span>HomePage container</span>
+
+      <Label>Name</Label>
+      <FormInput>Quynhdtn</FormInput>
     </>
   );
 }
