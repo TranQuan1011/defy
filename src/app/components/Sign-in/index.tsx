090fde94aa32ff677f2efa8ed865e52b75921463
Vu Thu Ha <havtt@dol-tech.com>
2021-10-06 21:18:03 +0700

    sign-in

--- src/app/components/Sign-in/index.tsx
+++ src/app/components/Sign-in/index.tsx
@@ -4,7 +4,11 @@ import { makeStyles } from '@mui/styles';
 import Input from 'app/components/Input/index';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { BackgroundButton } from 'app/components/Button/index';
+import { ButtonColor } from 'app/components/Button/index';
+import { SxProps } from '@mui/system';
+import { Theme, InputAdornment,IconButton } from '@mui/material';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import VisibilityIcon from '@mui/icons-material/Visibility';
 
 const useStyles = makeStyles({
     text: {
@@ -28,32 +32,57 @@ const useStyles = makeStyles({
             <Input 
                 label='Name' type='name' 
                 placeholder='Enter Name' 
+                sx={input}
             />
             <Input 
                 label='Email' 
                 type='email' 
                 placeholder='Enter Email' 
+                sx={input}
             />
             <Input 
                 label='Password' 
                 type='password' 
                 placeholder='Enter Password' 
+                sx={input}
+                InputProps= {{
+                    endAdornment: (
+                        <InputAdornment position="end">
+                        <IconButton edge="end">
+                            <VisibilityIcon />
+                        </IconButton>
+                        </InputAdornment>
+                    ),
+                }}
             />
             <Input 
                 label='Confirm password' 
                 type='password'
                 placeholder='Enter Confirm password' 
+                sx={input}
+                InputProps= {{
+                    endAdornment: (
+                        <InputAdornment position="end">
+                        <IconButton edge="end">
+                            <VisibilityOffIcon />
+                        </IconButton>
+                        </InputAdornment>
+                    ),
+                }}
             />
             <Typography className={classes.text}>We will not share or sell your information to 3rd parties.</Typography>
             <Typography className={classes.text}>By clicking on <span className={classes.span}>Create Account</span>, you agree to DeFi For
                 You’s Terms and Conditions of Use.</Typography>
-            <BackgroundButton 
+            <ButtonColor 
                 className={classes.btn}
             >
                 Create Account
-            </BackgroundButton>
+            </ButtonColor>
         </Box>
     </>
   );
 }
 
+const input: SxProps<Theme> = {
+    marginBottom: '24px',
+}
