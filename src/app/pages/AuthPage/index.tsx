793e66c7020caff9553febf9ce3f21af606dbbb5
Trần Mạnh Quân <tranmanhquan10111999@gmail.com>
2021-10-06 10:35:38 +0700

    Sua font h1,h2 va spacing grid

--- src/app/pages/AuthPage/index.tsx
+++ src/app/pages/AuthPage/index.tsx
@@ -14,7 +14,7 @@ export default function AuthPage() {
   return (
     <Box component="main" padding="20px 16px">
       <StyledContainer>
-        <Typography component="h1" sx={h1} gutterBottom>
+        <Typography component="h1" sx={h1}>
           Exclusive on <DefySpan>DeFi For You.</DefySpan>
         </Typography>
         <Typography component="h2" variant="subtitle1" sx={h2}>
@@ -23,7 +23,7 @@ export default function AuthPage() {
           to <DefySpan>31/12/2021</DefySpan> to have 100% chance of receiving
           DFY tokens
         </Typography>
-        <Grid container rowSpacing={3}>
+        <Grid container rowSpacing={3} columnSpacing={11}>
           <Grid item xs={12} md={6} order={{ xs: 2, md: 1 }}>
             <Tabs
               value={0}
@@ -53,33 +53,44 @@ const DefySpan = styled('span')(({ theme }) => ({
 
 const StyledTab = styled(Tab)(({ theme }) => ({
   fontWeight: 600,
-  [theme.breakpoints.up('md')]: {
+  [theme.breakpoints.up('lg')]: {
     fontSize: '2rem',
   },
   lineHeight: '37px',
 }));
 
 const StyledContainer = styled(Container)(({ theme }) => ({
-  maxWidth: '100%',
+  width: '100%',
+  maxWidth: theme.breakpoints.values['xl'],
   [theme.breakpoints.up('lg')]: {
-    maxWidth: '90%',
+    width: '80%',
   },
 }));
 
 const h1: SxProps<Theme> = {
   fontWeight: 'bold',
-  fontSize: '36px',
-  lineHeight: '44px',
+  fontSize: {
+    xs: '2.5rem',
+    lg: '3.8rem',
+  },
 };
 
 const h2: SxProps<Theme> = {
   fontWeight: theme => theme.typography.fontWeightMedium,
-  lineHeight: '17px',
-  marginBottom: '32px',
+  fontSize: {
+    lg: '1.4rem',
+  },
+  marginBottom: {
+    xs: '2.3rem',
+    lg: '3.5rem',
+  },
 };
 
 const tabs: SxProps<Theme> = {
   '& .MuiTabs-flexContainer': {
     columnGap: '3.2rem',
+    '& button': {
+      textTransform: 'none',
+    },
   },
 };
