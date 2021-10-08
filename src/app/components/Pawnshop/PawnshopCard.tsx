f3283d5f3fe1173ea0d0dd934b0be38f6d43664b
Vu Thu Ha <havtt@dol-tech.com>
2021-10-08 11:51:19 +0700

    pawnshop

---
+++ src/app/components/Pawnshop/PawnshopCard.tsx
@@ -0,0 +1,110 @@
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import StarIcon from '@mui/icons-material/Star';
+import { SxProps } from '@mui/system';
+import { Theme } from '@mui/material';
+
+interface IProps {
+  image?: string;
+  description?: string;
+  content?: string;
+}
+
+export default function PawnshopCard(props: IProps) {
+  const { image, description, content } = props;
+  return (
+    <Box sx={box}>
+      <Grid sx={grid}>
+        <img alt="img" src={image} className="img" />
+        <Typography sx={superShop}>
+          Super shop
+        </Typography>
+        <Typography sx={rate}>
+            <StarIcon sx={star}/>
+          1000
+        </Typography>
+      </Grid>
+      <Grid>
+        <Typography sx={des}>{description}</Typography>
+        <Typography sx={cont}>{content}</Typography>
+      </Grid>
+    </Box>
+  );
+}
+
+const box: SxProps<Theme> = {
+  padding: '0 10px',
+  '@media (max-width: 750px)': {
+    maxWidth: '300px !important',
+    margin: '0 auto',
+    '& .img': {
+      maxWidth: '283px',
+    }
+  },
+  '@media (min-width: 1028px) and (max-width: 1080px)': {
+    maxWidth: '428px',
+    margin:' 0 auto',
+  },
+}
+const grid: SxProps<Theme> = {
+  position: 'relative',
+  marginBottom: '12px',
+}
+const superShop: SxProps<Theme> = {
+  padding: '5px 12px',
+  background: '#ff4848',
+  borderRadius: '15.5px',
+  color: '#ffffff',
+  fontWeight: '500',
+  fontSize: '14px',
+  lineHeight: '17px',
+  position: 'absolute',
+  top: '12px',
+  left: '12px',
+  '@media (max-width: 750px)': {
+    fontSize: '12px',
+    lineHeight: '15px',
+  },
+}
+const rate: SxProps<Theme> = {
+  position: 'absolute',
+  fontSize: '14px',
+  borderRadius: '16px',
+  background: '#f8b017',
+  color: '#ffffff',
+  textAlign: 'center',
+  fontWeight: '500',
+  lineHeight: '17px',
+  top: '12px',
+  right: '12px',
+  padding: '5px 10px 3px 5px',
+  '@media (max-width: 750px)': {
+    fontSize: '12px',
+    lineHeight: '15px',
+    padding: '5px 8px 3px 5px',
+  },
+}
+const star: SxProps<Theme> = {
+  marginBottom:'3px',
+  height: '15.5px',
+  color: '#ffffff',
+}
+const des: SxProps<Theme> = {
+  fontSize: '20px',
+  lineHeight: '24px',
+  marginBottom: '8px',
+  '@media (max-width: 750px)': {
+    fontSize: '18px',
+    lineHeight: '22px',
+    marginBottom: '4px',
+  },
+}
+const cont: SxProps<Theme> = {
+  fontSize: '16px',
+  lineHeight: '20px',
+  '@media (max-width: 750px)': {
+    fontSize: '14px',
+    lineHeight: '17px',
+  },
+}
