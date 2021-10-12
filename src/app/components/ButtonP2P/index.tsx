b798b8c6a0e13b1fbd77841fed0a7b90156d2d39
Vu Thu Ha <havtt@dol-tech.com>
2021-10-12 13:09:17 +0700

    buttonP2P

--- src/app/components/ButtonP2P/index.tsx
+++ src/app/components/ButtonP2P/index.tsx
@@ -5,12 +5,18 @@ export const ButtonP2P = styled(Button)`
   border: 1px solid #232732;
   border-radius: 9px;
   color: #ffffff;
-  font-size: 14px;
-  line-height: 17px;
+  font-size: 16px;
+  line-height: 20px;
   font-weight: 500;
   background: #232732;
-  padding: 10px 8px 11px 8px;
+  padding: 15px 33px;
   text-transform: none;
+  &:hover{
+    background: rgba(219, 168, 61, 0.1);
+    border: 1px solid #DBA83D;
+    box-sizing: border-box;
+    border-radius: 2px;
+  }
   @media (max-width: 600px){
     font-size: 12px;
     line-height: 15px;
