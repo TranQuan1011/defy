21577c17a897a5ca30b2fb415f63924c4fce210f
Vu Thu Ha <havtt@dol-tech.com>
2021-10-12 12:55:46 +0700

    buttonP2P

---
+++ src/app/components/ButtonP2P/index.tsx
@@ -0,0 +1,18 @@
+import { styled } from '@mui/material/styles';
+import { Button } from '@mui/material';
+
+export const ButtonP2P = styled(Button)`
+  border: 1px solid #232732;
+  border-radius: 9px;
+  color: #ffffff;
+  font-size: 14px;
+  line-height: 17px;
+  font-weight: 500;
+  background: #232732;
+  padding: 10px 8px 11px 8px;
+  text-transform: none;
+  @media (max-width: 600px){
+    font-size: 12px;
+    line-height: 15px;
+  }
+`;
